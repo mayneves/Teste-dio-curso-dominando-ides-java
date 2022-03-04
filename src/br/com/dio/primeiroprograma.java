381639e177e9d77c7679556ea58fe34751379f82
Mayraneves <mayraneves29@gmail.com>
2022-03-04 20:37:37 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		gato gato = new  gato();
+		Livro livro = new 	Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/*int a = 2;
+		int b = 3;
+	System.out.println(("Hello World ") + (a+b));*/
+
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String npag;
+}
